e4bd1069fe1d628d126abc2c16bdcefa28cffbfc
kyle <sdfcvbjkl@gmail.com>
2022-03-16 17:38:16 +0800

    style: 用 bootstrap navbar 重製導覽列

--- src/views/BackendView.vue
+++ src/views/BackendView.vue
@@ -1,9 +1,32 @@
 <template>
-  <h1>後台首頁</h1>
-  <div id="nav">
-    <router-link to="/">回前台首頁</router-link> |
-    <a href="#" @click.prevent="logout">登出</a>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">後台</a>
+      <!-- 行動版漢堡選單 -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">回首頁</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" @click.prevent="logout" to="/">登出</router-link>
+            <!-- <a href="#" class="nav-link" @click.prevent="logout">登出</a> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1>前台首頁</h1>
-  <div id="nav">
-    <router-link to="/">首頁</router-link> |
-    <router-link to="/products">產品列表</router-link> |
-    <router-link to="/cart">購物車</router-link> |
-    <router-link to="/admin/products">後台登入</router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand">前台</a>
+      <!-- 行動版漢堡選單 -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/products">產品列表</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/cart">購物車</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/admin/products">後台登入</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
 
