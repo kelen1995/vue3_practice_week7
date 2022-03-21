191dc75870e5c53cfc9072a9f4cde940f1265414
kyle <sdfcvbjkl@gmail.com>
2022-03-21 12:41:18 +0800

    chore: 修改網站標題 & icon

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue3_practice_week7/'
     : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'KL Backery',
+    },
+  },
 };
